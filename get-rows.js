d9bd626a5166b3d452ce51bbcb3b8b19093c9fc9
Crystal K Young <crystalyoung@Crystals-MacBook-Pro.local>
2020-07-17 12:00:51 -0400

    add

--- get-rows.js
+++ get-rows.js
@@ -1,5 +1,17 @@
-
-
+// * `getRows` - takes in newline-separated string and convert to 2d array
+//   * HINT: This is a mapping function, essentially. Make a new array, split the huge csv string, loop through the array that gives you, and push an array for each string row into your new array.
+//   * HINT: You can use `getRow` (requiring it in!) to convert each row from a string to an array.
+const getRow = require('./get-row.js');
+const getRows = function(str) {
+  const row = [];
+  //const strIndex = [];
+  const strInd = str.split('\n');
+  //for ( let i = 0; i < str.length; i++) {
+   // if (str[i] === ',') {
+  for ( const strIndex of strInd) { 
+      rows.push(getRow(strIndex));
+    }
+        return row;
 
 
 
