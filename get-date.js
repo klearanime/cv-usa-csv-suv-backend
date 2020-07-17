072565a7b8e68532ee7f69e62b8c22d1294c8ad0
Crystal K Young <crystalyoung@Crystals-MacBook-Pro.local>
2020-07-17 10:21:01 -0400

    add

--- get-date.js
+++ get-date.js
@@ -1,3 +1,12 @@
+// * `getDate` - takes in a row array and returns the date from it (see the table header in the `.csv` file for which that is!)
+
+const getDate = function(date){
+  // for (const row of date) {
+   // const date = date.split(',');
+  return date[3];
+}
+
+
 
 
 
@@ -6,4 +15,6 @@ if (typeof getDate === 'undefined') {
 }
 
 
+
 module.exports = getDate;
+
