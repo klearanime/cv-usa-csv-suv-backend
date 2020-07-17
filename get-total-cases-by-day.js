a8782cc018508100496c4fcd61b71740294fc687
Crystal K Young <crystalyoung@Crystals-MacBook-Pro.local>
2020-07-17 12:04:09 -0400

    add

--- get-total-cases-by-day.js
+++ get-total-cases-by-day.js
@@ -1,6 +1,14 @@
+// * `getNewCasesByDay` - takes in a date string and a 2D array and returns new cases that day
+//   * HINT: You'll want to loop through each row of the data!
+//   * HINT: You can use `getDate` and `getNewCases` so you don't have to check those indices again!
 
-
-
+const getTotalCasesByDay = function(date, arr) {
+  for ( const newCases of arr) {
+    if ( newCases[3] === date) {
+      return newCases[4]
+    }
+  }
+}
 
 
 
