a9516bcb665e64ab06f70c3010edf514a1729da6
Crystal K Young <crystalyoung@Crystals-MacBook-Pro.local>
2020-07-17 12:01:42 -0400

    add

--- get-row.js
+++ get-row.js
@@ -1,4 +1,17 @@
+// * `getRow` - takes in a comma-separated string and converts it to array, returning that array
 
+
+
+// const getRow = function(rows) {
+//   for ( const row of rows) {
+//     const cells = row.slice(',');
+//     console.log(cells)//(cells[])
+//   }
+// }
+
+const getRow = function(str) {
+  return str.split(',')
+}
 
 
 
@@ -9,4 +22,32 @@ if (typeof getRow === 'undefined') {
 }
 
 
+
 module.exports = getRow;
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const rows = file.split('\n')
+
+//console.log(rows)
+//console.log(rows.slice[2])
+
+// for ( let i = 0; 39 < rows.length; i++) {
+//const getRow = function(rows) {
+//  for (const row of rows) {
+//      const cells = row.split(',');
+//      console.log()//(cells[]
+// )
+//  }
+//  }
+
