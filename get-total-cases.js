9a634c12c589d20e1071313e867dcc10223319b2
Crystal K Young <crystalyoung@Crystals-MacBook-Pro.local>
2020-07-17 12:04:44 -0400

    add

--- get-total-cases.js
+++ get-total-cases.js
@@ -1,6 +1,8 @@
+// * `getTotalCases` - takes in a row array and returns the total cases from it (see the table header for which that is!)
 
-
-
+const getTotalCases = function(row) {
+  return row[4];
+}
 
 
 
@@ -12,3 +14,6 @@ if (typeof getTotalCases === 'undefined') {
 
 
 module.exports = getTotalCases;
+
+
+
